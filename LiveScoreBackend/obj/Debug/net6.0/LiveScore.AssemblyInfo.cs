a5197f10796641732c3a82031c9cd4992f33c8d0
BhagyashreeDungarwal <bhagyashreekdungarwal@gmail.com>
2024-02-16 22:46:41 +0530

    minor

--- LiveScoreBackend/obj/Debug/net6.0/LiveScore.AssemblyInfo.cs
+++ LiveScoreBackend/obj/Debug/net6.0/LiveScore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LiveScore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68dfe50c6a560d0298e7bf41eb57fe6fac5454b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LiveScore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LiveScore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
