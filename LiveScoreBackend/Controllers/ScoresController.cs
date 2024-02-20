b216c96180f00f87d9535f9b3aefc02b0c8e1444
BhagyashreeDungarwal <bhagyashreekdungarwal@gmail.com>
2024-02-20 16:43:43 +0530

    FrontendReact

---
+++ LiveScoreBackend/Controllers/ScoresController.cs
@@ -0,0 +1,20 @@
+﻿using LiveScore.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveScore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ScoresController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
